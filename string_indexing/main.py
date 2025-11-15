c094592945dee32cf3d06a994703b4ad9b4fd041
elKINTARO <>
2025-11-15 19:35:05 +0000

    String Indexing

---
+++ string_indexing/main.py
@@ -0,0 +1,9 @@
+grocery_item = "Grilled Chicken Salad"
+length_of_item = len(grocery_item)
+first_word, second_word, third_word = "Grilled", "Chicken", "Salad"
+first_char, second_char, third_char = first_word[0], second_word[0], third_word[0]
+last_char1, last_char2, last_char3 = first_word[-1], second_word[-1], third_word[-1]
+# Testing
+print("Length of item name:", length_of_item)
+print("First character of each word:", first_char, second_char, third_char)
+print("Last character of each word:", last_char1, last_char2, last_char3)
