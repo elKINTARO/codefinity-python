80a7b0ad41756e76a922d76e64e32a59f955164c
elKINTARO <>
2025-11-18 15:42:55 +0000

    Завдання: Підсумковий Проєкт Із Сортування Інвентарю

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-# Calculate multiplication
-print(7.32 * 0.2) 
-
-# Calculate division
-print(8.30 / 20)
+# Lists of items and categories for slicing
+items = "Bubblegum, Chocolate, Pasta"
+categories = "Candy Aisle, Pasta Aisle"
+candy1 = items[0:9]
+candy2 = items[11:20]
+dry_goods = items[22:28]
+category1 = categories[0:11]
+category2 = categories[13:24]
+bubblegum_price = "$1.50"
+chocolate_price = "$2.00"
+pasta_price = "$5.40"
+print(f"We have {candy1} for {bubblegum_price} in the {category1}")
+print(f"We have {candy2} for {chocolate_price} in the {category1}")
+print(f"We have {dry_goods} for {pasta_price} in the {category2}")
