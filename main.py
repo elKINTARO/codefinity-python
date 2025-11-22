256ca81a6c9948e69853bd301415f4318ba2092b
elKINTARO <>
2025-11-22 20:59:15 +0000

    Завдання: Операції з Кортежами

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-meat = ["Ham", 3.99, 50, "Sliced"]
-cheese = ["Cheddar", 5.49, 100, "Sharp"]
-condiment = ["Mustard", 1.99, 75, "Spicy"]
+# Current inventory on shelf
+shelf = ("apples", "oranges", "bananas", "apples", "grapes", "bananas", "apples")
+apple_count = shelf.count("apples")
+print("Number of Apples:", apple_count)
+banana_index = shelf.index("bananas")
+print("First Banana Index:", banana_index)
+if apple_count < 5:
+    print("Apples need to be restocked.")
+else:
+    print("Apples are sufficiently stocked.")
+grapes_count = shelf.count("grapes")
+if grapes_count == 1:
+    print("Grapes need to be restocked.")
+else:
+    print("Grapes are sufficiently stocked.")
 
-deli_dept = [meat, cheese, condiment]
-print("Initial Deli List:", deli_dept)
-
-if "Ham" in meat and meat[2] < 100:
-    meat[2] = 100
-
-seasonal = ["Turkey", 4.50, 100, "Sliced"]
-deli_dept.append(seasonal)
-deli_dept.remove(condiment)
-deli_dept.sort()
-
-print("Updated Deli List:", deli_dept)
+if "oranges" in shelf:
+    print("Oranges are at index:", shelf.index("oranges"))
+else:
+   print("Oranges are out of stock.") 
